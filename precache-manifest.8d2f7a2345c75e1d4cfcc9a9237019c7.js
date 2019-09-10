401fec2a3aa5963453f8cc479ab2b6d80c588702
wangliting <wangliting@e-autolife.com>
2019-09-10 18:19:13 +0800

    web:大后台2.0

---
+++ precache-manifest.8d2f7a2345c75e1d4cfcc9a9237019c7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c360b9814e7f571c4eac07a6bcbd578",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e5fefc7878bf6af9713b",
+    "url": "./static/css/2.3967ed31.chunk.css"
+  },
+  {
+    "revision": "5c585b7c6ef67f6586b0",
+    "url": "./static/css/main.628ca08a.chunk.css"
+  },
+  {
+    "revision": "e5fefc7878bf6af9713b",
+    "url": "./static/js/2.0769b3aa.chunk.js"
+  },
+  {
+    "revision": "5c585b7c6ef67f6586b0",
+    "url": "./static/js/main.b18dbe3d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "c002787e167d30bd50f4a6dbfc443e46",
+    "url": "./static/media/arrowLf.c002787e.png"
+  },
+  {
+    "revision": "1096fe46dc6d5e659cd892e38b267acf",
+    "url": "./static/media/bg.1096fe46.png"
+  },
+  {
+    "revision": "4d8f59f2c95014a76621f4bfc33cf3c3",
+    "url": "./static/media/logo.4d8f59f2.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "./static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "./static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "./static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "./static/media/slick.f97e3bbf.svg"
+  }
+]);
